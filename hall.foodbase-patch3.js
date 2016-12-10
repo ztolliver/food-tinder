a4420fed313cc1f3a0994d44f3a1127589d4586b
Elizabeth Hall <elizabethhall205@gmail.com>
2016-12-10 14:41:14 -0500

    hall.foodbase-luch-patch(1).js

---
+++ hall.foodbase-patch3.js
@@ -0,0 +1,32 @@
+//filters, age, location, platonic or ....
+//collection with key:value types
+
+//var keyValue list 
+//Profile 
+survey w drop down list 
+
+var d {
+
+chicken: "chicken",
+taco: "taco",
+pizza: "pizza",
+
+//declar Global var
+//var= emptyList
+
+var lunchFoodList =  [
+//
+	tuna: "tuna", //key1
+	pb: "pb",//key2
+	cheese: "cheese", //key3
+	soup: "soup",//key4
+	salad: "salad",//key5
+ ];
+
+// var arr = newArray();
+
+ //collect the first thing out of it (dinner)
+	//collect second thing out of it(lunch)
+		//collect third thing out of it(platonic)
+	
+
